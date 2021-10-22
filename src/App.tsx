1ebe5e530b679449446cf011ffe36e48e6ec0d0d
Marinos33 <loicmidavaine@gmail.com>
2021-10-22 18:20:31 +0200

    WIP-Youtubedl

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import youtubedl from'youtube-dl-exec';
 
 function App() {
+const getInfo = async () => {
+  const info = youtubedl("https://www.youtube.com/watch?v=knk56D-ciMw", {dumpJson: true});
+  console.log(info);
+}
+getInfo();
   return (
     <div className="App">
       <header className="App-header">
