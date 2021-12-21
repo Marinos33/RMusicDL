bd1e72cdd41a3d9da86a2f7674c68132f6ee26e1
Marinos33 <loicmidavaine@gmail.com>
2021-12-21 16:02:16 +0100

    WIP - settings

---
+++ src/renderer/components/settings/SideBar/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@mui/material';
+import React from 'react';
+
+const SettingsSideBar: React.FC = () => {
+  return (
+  );
+};
+
+export default SettingsSideBar;
--- src/renderer/pages/Settings/index.tsx
+++ src/renderer/pages/Settings/index.tsx
@@ -1,9 +1,10 @@
+import { Container } from '@mui/material';
 import SettingsSideBar from '@src/renderer/components/settings/SideBar';
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
 const SettingsPage = () => {
-  return <SettingsSideBar />;
+  return <Container maxWidth={false} sx={{ overflow: 'hidden' }}><SettingsSideBar /></Container>;
 };
 
 export default hot(module)(SettingsPage);
