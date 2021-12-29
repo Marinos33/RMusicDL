000a23d919bde7a4ab15cf28a0ed9f5501335756
Marinos33 <loicmidavaine@gmail.com>
2021-12-29 15:22:29 +0100

    fix

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,7 +19,6 @@ import { PlaylistRepository } from './Database/Repository/PlaylistRepository';
 import { ProfileRepository } from './Database/Repository/DownloadingProfileRepository';
 import isDev from 'electron-is-dev';
 import log from 'electron-log';
-import path from 'path';
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
