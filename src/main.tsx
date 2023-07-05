5cf568a823b87cb2565cecade58555964d92af3f
Marinos33 <loicmidavaine@gmail.com>
2023-07-05 15:21:26 +0200

    wip - theming

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { ConfigProvider } from 'antd';
+import { theme as myTheme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <ConfigProvider theme={myTheme}>
+        <RouterProvider router={router} />
+      </ConfigProvider>
     </Provider>
   </React.StrictMode>,
 );
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  token: {
+    colorPrimary: '#145bbf',
+    colorSuccess: '#269a0f',
+    colorWarning: '#c58911',
+    colorError: '#cd1014',
+    colorInfo: '#10c2e2',
+    fontSize: 16,
+    borderRadius: 12,
+  },
+};
