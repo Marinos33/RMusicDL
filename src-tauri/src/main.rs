e4f6037e81fea0dff7fa6ba757f24e7925b5a9ee
Marinos33 <loicmidavaine@gmail.com>
2023-07-10 14:53:26 +0200

    added youtube-dl download

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use youtube_dl::{download_yt_dlp, YoutubeDl};//yt-dlp downloader
-use tokio::runtime::Runtime;
-use tauri::Builder;
 use std::sync::{Arc, Mutex};
+use std::path::Path;
 
 // Define a global variable to hold the result
 static YTDLPPATH: once_cell::sync::Lazy<Arc<Mutex<Option<String>>>> =
@@ -34,12 +33,29 @@ fn get_playlist_info(url: &str) -> String {
 }
 
 async fn init() {
-    let yt_dlp_path: Result<std::path::PathBuf, youtube_dl::Error> = download_yt_dlp(".").await;
-    //convert yt_dlp_path to string
-    let path = yt_dlp_path.unwrap().to_str().unwrap().to_string();
-    println!("yt-dlp downloaded to {:?}", path);
 
-    *YTDLPPATH.lock().unwrap() = Some(path);
+    let yt_dlp_path = Path::new("./yt-dlp.exe");
+
+    if yt_dlp_path.exists() {
+        println!("yt-dlp already downloaded");
+        *YTDLPPATH.lock().unwrap() = Some(yt_dlp_path.to_str().unwrap().to_string());
+        return;
+    }
+
+    let yt_dlp_download_result: Result<std::path::PathBuf, youtube_dl::Error> = download_yt_dlp(".").await;
+
+    match yt_dlp_download_result {
+        Ok(path) => {
+            //convert yt_dlp_path to string
+            let path = path.to_str().unwrap().to_string();
+            println!("yt-dlp downloaded to {:?}", path);
+
+            *YTDLPPATH.lock().unwrap() = Some(path);
+        },
+        Err(e) => {
+            eprintln!("Failed to download yt-dlp: {:?}", e);
+        },
+    }
 }
 
 #[tokio::main]
