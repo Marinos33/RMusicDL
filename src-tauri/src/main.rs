019c5e9ba8ed8b95fffa8def7c3d51485438cb4a
Marinos33 <loicmidavaine@gmail.com>
2023-07-11 15:17:21 +0200

    added ffmpeg

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,26 @@
 
 use youtube_dl::{download_yt_dlp, YoutubeDl};//yt-dlp downloader
 use std::sync::{Arc, Mutex};
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::io::copy;
+use ffmpeg_sidecar::{
+    command::ffmpeg_is_installed,
+    download::{check_latest_version, download_ffmpeg_package, ffmpeg_download_url, unpack_ffmpeg},
+    error::Result as ffmpeg_result,
+    paths::sidecar_dir,
+    version::ffmpeg_version,
+  };
+use reqwest::Client;
+use url::Url;
 
 mod types;
 use types::Playlist;
 
 // Define a global variable to hold the result
 static YTDLPPATH: once_cell::sync::Lazy<Arc<Mutex<Option<String>>>> =
+    once_cell::sync::Lazy::new(|| Arc::new(Mutex::new(None)));
+static FFMPEGPATH: once_cell::sync::Lazy<Arc<Mutex<Option<String>>>> =
     once_cell::sync::Lazy::new(|| Arc::new(Mutex::new(None)));
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -57,7 +70,7 @@ async fn download_playlist(url: String, format: String) -> bool {
 
     let path: String = YTDLPPATH.lock().unwrap().clone().unwrap();
     let format_arg: String = format!("--audio-format {}", format);
-    let ffmpeg_path: String = ;
+    let ffmpeg_path: String = "".to_string();
     let ffmpeg_arg: String = format!("--ffmpeg-location {}", ffmpeg_path);
 
     if let Err(error) = YoutubeDl::new(url)
@@ -79,9 +92,9 @@ async fn download_playlist(url: String, format: String) -> bool {
     return true;
 }
 
-async fn init() {
+async fn init_ytdlp() {
 
-    let yt_dlp_path = Path::new("./yt-dlp.exe");
+    let yt_dlp_path: &Path = Path::new("./yt-dlp.exe");
 
     if yt_dlp_path.exists() {
         println!("yt-dlp already downloaded");
@@ -105,9 +118,53 @@ async fn init() {
     }
 }
 
+async fn init_ffmpeg() -> Result<(), Box<dyn std::error::Error>> {
+
+    let download_url: &str = ffmpeg_download_url()?;
+    println!("Download URL: {}", download_url);
+
+        // Extract the filename from the download URL
+        let zip_name = "ffmpeg-release-essentials.zip";
+        let filename = "ffmpeg.exe";
+        let destination: &Path = Path::new(".");
+        let exe_path: PathBuf = destination.join(filename);
+
+        // Check if ffmpeg is already installed
+        if exe_path.exists() {
+            println!("ffmpeg is already installed");
+            *FFMPEGPATH.lock().unwrap() = Some(exe_path.to_str().unwrap().to_string());
+            return Ok(());
+        }
+
+        // Download the file
+        let client: Client = Client::new();
+        let response: reqwest::Response = client.get(download_url).send().await?;
+        // Check if the download was successful
+        if response.status().is_success() {
+            // Save the file to disk
+            let mut file: File = File::create(zip_name)?;
+            copy(&mut response.bytes().await?.as_ref(), &mut file)?;
+
+            let archive_path: PathBuf = destination.join(&zip_name).canonicalize()?;
+
+            println!("Destination: {:?}", destination);
+            // Unpack the file
+            println!("Extracting...");
+            unpack_ffmpeg(&archive_path, &destination)?;
+
+            *FFMPEGPATH.lock().unwrap() = Some(exe_path.to_str().unwrap().to_string());
+            println!("Done! 🏁");
+        } else {
+            println!("Failed to download file: {:?}", response.status());
+        }
+
+    Ok(())
+  }
+
 #[tokio::main]
 async fn main() {
-    init().await;
+    init_ytdlp().await;
+    _ = init_ffmpeg().await;
     
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])    
